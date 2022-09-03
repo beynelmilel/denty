82eca0d943b4fa12122365f79428a5287d4438a2
Muhammed Melih GÜNDOĞAN <muhammedmelihgundogan@gmail.com>
2022-09-03 14:59:39 +0300

    Replaced with merged code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import 'package:denty/view/auth/sign_in/view/sign_in_view.dart';
-import 'package:denty/view/doctors/detail/appointment/appointment_view.dart';
-import 'package:denty/view/user/home/view/home_view.dart';
-=======
->>>>>>> 7799cfd785c56b751454cc01a3baf169473d80c8
+
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,8 +10,7 @@ import 'core/provider/user_provider.dart';
 import 'core/routes/route.dart';
 import 'core/theme/theme.dart';
 import 'firebase_options.dart';
-import 'utils/constants/router_constants.dart';
-import 'view/doctors/add_doctor/add_doctors_view.dart';
+import 'view/doctors/detail/appointment/appointment_view.dart';
 
 Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(
